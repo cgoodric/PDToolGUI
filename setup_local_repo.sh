6b45a677dd2e27e87f9a60664b0d7c76f886f71f
Calvin Goodrich <cgoodric@yahoo.com>
2014-06-09 18:17:55 -0700

    Cleanup and docs reorg

--- setup_local_repo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# add CIS libraries to local repository
-PWD=`pwd`
-
-mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file -Dfile=lib/cscommon-2230.jar -DgroupId=com.compositesw -DartifactId=common -Dversion=6.2.2 -Dpackaging=jar -DlocalRepositoryPath="$PWD/repo/"
